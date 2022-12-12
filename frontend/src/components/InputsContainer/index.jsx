6cd8420f64a0d948b65d2c5c179281ff0ec00aa4
BernardoModerno <bernardo.moderno@gmail.com>
2022-12-12 15:18:25 -0300

    Cadastro de finança, InfoContainer, front e back

--- frontend/src/components/InputsContainer/index.jsx
+++ frontend/src/components/InputsContainer/index.jsx
@@ -1,34 +1,82 @@
+import { useEffect } from 'react';
+import { useState } from 'react'
+import { api } from '../../services/api';
 import './styles.css'
 
-export const InputsContainer = () => {
+export const InputsContainer = ({ addItens }) => {
+
+    const [data, setData] = useState('');
+    const [categoria_id, setCategoriaId] = useState('');
+    const [titulo, setTitulo] = useState('');
+    const [valor, setValor] = useState('');
+
+    const [nameCategoria, setNameCategoria] = useState([]);
+
+    useEffect(() => {
+
+        api.get('/listar/categoria/0').then(response => {
+            console.log(response.data.rows);
+            setNameCategoria(response.data.rows)
+        })
+
+    },[])
+
+    function handleAddItens() {
+        const dados = {
+            data,
+            categoria_id,
+            titulo,
+            valor
+        }
+        addItens({dados})
+    }
+
     return (
         <section className="inputs-container">
 
             <div className="container-input">
                 <label htmlFor='data'>Data</label>
-                <input type="date" />
+                <input 
+                    type="date"
+                    value={data}
+                    onChange={e => setData(e.target.value)}
+                />
             </div>
 
             <div className="container-input">
                 <label htmlFor='category'>Categoria</label>
-                <select name="category" id="category">
-                    <option value='Ganho'>Ganho</option>
-                    <option value='Despesas'>Despesas</option>
+                <select 
+                    name="category" 
+                    id="category"
+                    value={categoria_id}
+                    onChange={e => setCategoriaId(e.target.value)}
+                >
+                    {nameCategoria.map(item => (
+                        <option key={item.id} value={item.id}>{item.descricao}</option>
+                    ))}
                 </select>
             </div>
 
             <div className="container-input">
                 <label htmlFor='title'>Título</label>
-                <input type="text" />
+                <input 
+                    type="text"
+                    value={titulo}
+                    onChange={e => setTitulo(e.target.value)} 
+                />
             </div>
 
             <div className="container-input">
                 <label htmlFor='value'>Valor</label>
-                <input type="number" />
+                <input 
+                    type="number"
+                    value={valor}
+                    onChange={e => setValor(e.target.value)} 
+                />
             </div>
 
             <div className="container-input">
-                <button>Adicionar</button>
+                <button onClick={handleAddItens} >Adicionar</button>
             </div>
 
         </section>
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -2,14 +2,23 @@ import { Header } from "../../components/Header"
 import { Information } from "../../components/Information"
 import { InfoTable } from "../../components/InfoTable"
 import { InputsContainer } from "../../components/InputsContainer"
+import { api } from "../../services/api"
 import './styles.css'
 
 export const Home = () => {
+
+    function handleSalveItens(item) {
+        const data = item.dados
+        api.post('/criar/financa', data).then(response => {
+            console.log(response);
+        })
+    }
+
     return (
         <div className="container">
             <Header />
             <Information />
-            <InputsContainer />
+            <InputsContainer addItens={handleSalveItens} />
             <InfoTable />
         </div>
     )
